01bab46b5a5e32e9f39ae5c8dcd03181ba69ec63
xomiakow <xomiakow.work@gmail.com>
2025-08-01 13:01:01 +0500

    Project uploading

---
+++ Images_Loader/Images_Loader/LoaderPanel.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Images_Loader
+{
+    public partial class LoaderPanel : UserControl
+    {
+        private const int BufferSize = 8192;
+
+        public Action<long>? onChunkDownloaded;
+        private CancellationTokenSource? _cancellationToken;
+
+        public LoaderPanel()
+        {
+            InitializeComponent();
+        }
+
+        public async void StartLoading()
+        {
+            _cancellationToken = new CancellationTokenSource();
+            ClearLoader();
+
+            if (UrlTextBox.Text != string.Empty)
+            {
+                try
+                {
+                    using var client = new HttpClient();
+                    using var response = await client.GetAsync(UrlTextBox.Text, HttpCompletionOption.ResponseHeadersRead, _cancellationToken.Token);
+                    response.EnsureSuccessStatusCode();
+
+                    using var stream = await response.Content.ReadAsStreamAsync(_cancellationToken.Token);
+                    var buffer = new byte[BufferSize];
+                    using var memoryStream = new MemoryStream();
+
+                    var read = 0;
+                    while ((read = await stream.ReadAsync(buffer, _cancellationToken.Token)) > 0)
+                    {
+                        await memoryStream.WriteAsync(buffer, 0, read, _cancellationToken.Token);
+                        onChunkDownloaded?.Invoke(read);
+                    }
+
+                    memoryStream.Position = 0;
+                    var bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmap.StreamSource = memoryStream;
+                    bitmap.EndInit();
+                    bitmap.Freeze();
+
+                    ImageDisplay.Source = bitmap;
+                }
+                catch (OperationCanceledException)
+                {
+                    ErrorLabel.Content = "Загрузка отменена";
+                }
+                catch (Exception)
+                {
+                    ErrorLabel.Content = "Ошибка загрузки изображения";
+                }
+            }
+        }
+
+        public async Task<long?> ImageByteCountAsync()
+        {
+            try
+            {
+                using var client = new HttpClient();
+                using var request = new HttpRequestMessage(HttpMethod.Head, UrlTextBox.Text);
+                using var response = await client.SendAsync(request);
+
+                response.EnsureSuccessStatusCode();
+
+                return response.Content.Headers.ContentLength;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private void ClearLoader() 
+        {
+            ErrorLabel.Content = "";
+            ImageDisplay.Source = null;
+        }
+
+        private void ClearUrl()
+        {
+            UrlTextBox.Text = "";
+        }
+
+        private void StopLoading() 
+        {
+            _cancellationToken?.Cancel();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartLoading();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            StopLoading();
+        }
+
+        private void ClearUrlButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearUrl();
+        }
+    }
+}
---
+++ Images_Loader/Images_Loader/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+
+namespace Images_Loader
+{
+    public partial class MainWindow : Window
+    {
+        private long _volumeToLoad = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void StartAll()
+        {
+            LoadingProgressBar.Value = 0;
+            _volumeToLoad = 0;
+            LoaderPanel1.onChunkDownloaded = b => PercentCounter(b);
+            LoaderPanel2.onChunkDownloaded = b => PercentCounter(b);
+            LoaderPanel3.onChunkDownloaded = b => PercentCounter(b);
+
+            if (LoaderPanel1.UrlTextBox.Text != string.Empty)
+            {
+                var size = await LoaderPanel1.ImageByteCountAsync();
+                _volumeToLoad += size.Value;
+            }
+
+            if (LoaderPanel2.UrlTextBox.Text != string.Empty)
+            {
+                var size = await LoaderPanel2.ImageByteCountAsync();
+                _volumeToLoad += size.Value;
+            }
+
+            if (LoaderPanel3.UrlTextBox.Text != string.Empty)
+            {
+                var size = await LoaderPanel3.ImageByteCountAsync();
+                _volumeToLoad += size.Value;
+            }
+
+            LoaderPanel1.StartLoading();
+            LoaderPanel2.StartLoading();
+            LoaderPanel3.StartLoading();
+        }
+
+        private void StartAllButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartAll();
+        }
+
+        private void PercentCounter(long chunkSize)
+        {
+            double percent = (double)chunkSize / _volumeToLoad * 100;
+            LoadingProgressBar.Value += percent;
+        }
+    }
+}
